698c142829335cc3ef93b8bddf7f779803825543
AMROLI BCA <sbccas.amroli@gmail.com>
2023-09-23 20:51:01 +0530

    Change Launcher Icon

---
+++ app/src/main/java/com/example/practical_assignment/main_home.java
@@ -0,0 +1,14 @@
+package com.example.practical_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+    }
+}
